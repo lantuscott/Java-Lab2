07c90334f9c71d2b19fdfb454178da2f9517739e
lfarinhap <lfarinhap@linux-1qn5>
2016-09-27 20:40:39 -0500

    2nd commit, added some new classes

---
+++ HW1/src/extra_credits/Div.java
@@ -0,0 +1,26 @@
+package extra_credits;
+
+import java.util.Scanner;
+
+public class Div {
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("Enter a number: ");
+			
+			double number = input.nextDouble();
+			
+			double division = number/0;
+			
+			System.out.println("result -> " + division);
+			
+		}
+		
+
+	}
+	
+}
